3c7ba6c8374740c1dede1bc6047c09f8b2f97ccc
isaiahxs <isaiahsinnathamby@gmail.com>
2023-03-19 11:56:56 -0400

    made the snake move automatically

--- snake.js
+++ snake.js
@@ -8,6 +8,10 @@ const snake_border = 'darkblue';
     //first coordinate represents the snake's head
 let snake = [ {x: 250, y: 250}, {x: 240, y: 250}, {x: 230, y: 250}]
 
+//horizontal velocity
+let dx = 10;
+let dy = 0;
+
 //get the canvas element
 const snakeboard = document.getElementById('gameCanvas');
 //return a two dimensional drawing context
@@ -17,9 +21,17 @@ const snakeboard_ctx = gameCanvas.getContext('2d');
 main();
 
 //main function called repeatedly to keep the game running
+    //to move the snake how we want, we can add a slight delay between each call with setTimeout
+    //we also need to make sure to call drawSnake every time we call move_Snake, as shown below
+        //if we don't, we won't be able to see the intermediate steps that show the snake moving
 function main() {
-    clearCanvas();
-    drawSnake();
+    setTimeout(function onTick() {
+        clearCanvas();
+        move_snake();
+        drawSnake();
+        //call main again
+        main();
+    }, 100)
 }
 
 //draw a border around the canvas
@@ -49,4 +61,19 @@ function drawSnakePart(snakePart) {
     snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
     //draw a border around the snake part
     snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+
+//making the snake move automatically
+    //to make the snake move one step (10px) to the right, we can increase the x-coordinate of every part of the snake by 10px (dx = +10)
+    //to make the snake move one step to the left, we would decrease the x-coordinate of every part of the snake by 10px (dx = -10)
+    //vertical directions will be determined by dy (vertical velocity of the snake)
+function move_snake() {
+    //create a new head for the snake
+    const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+    // add the new head to the beginning of the snake
+    snake.unshift(head);
+    //remove the last element of the snake
+    snake.pop();
+        //this way, all the other snake parts shift into place
 }
